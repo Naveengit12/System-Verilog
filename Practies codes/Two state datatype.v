7b97e22dc3503240aa7d74c6436fca8105a5d249
Naveengit12 <jikkilinaveen@gmail.com>
2025-09-17 16:49:20 +0530

    Create Two state datatype.v

---
+++ Practies codes/Two state datatype.v
@@ -0,0 +1,12 @@
+module two_state_datatype;
+int a;
+shortint b;
+longint c;
+real f;
+bit e;
+byte d;
+
+initial begin
+$display(" a=%b, b=%b, c=%b, d=%b, e=%b, f=%f", a,b,c,d,e,f);
+end
+endmodule
