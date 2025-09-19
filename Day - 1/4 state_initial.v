561652c058a22518c33a6b9b12c2edf37ac557c7
Naveengit12 <jikkilinaveen@gmail.com>
2025-09-19 17:32:44 +0530

    Create 4 state_initial.v

---
+++ Day - 1/4 state_initial.v
@@ -0,0 +1,13 @@
+
+module four_state_datatype;
+  wire a;
+  reg b;
+  logic c;
+  time d;
+  integer e;
+  real f;
+
+  initial begin
+    $display("a=%b, b=%b, c=%b, d=%0d, e=%d,f=%0d", a, b, c, d, e, f);
+  end
+endmodule
