4761a89da4949dc0265ad1803a528e0f732a378c
Naveengit12 <jikkilinaveen@gmail.com>
2025-09-20 20:46:49 +0530

    Update 1D packed_array.v

--- Day - 2/1D packed_array.v
+++ Day - 2/1D packed_array.v
@@ -2,10 +2,13 @@ module packed_array_1D;
 
   // 1D packed array (vector) of 8 bits
   bit [7:0] packed_array = 8'hA5; 
+  integer i;
+
   initial begin
     $display("Packed 1D array bits:");
-    foreach (packed_array[i]) begin
+    for (i = 7; i >= 0; i--) begin
       $display("packed_array[%0d] = %b", i, packed_array[i]);
     end
   end
 endmodule
+
