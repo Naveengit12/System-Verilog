a0bf1a3487febc93c9bff7c910d94445168e0dd1
Naveengit12 <jikkilinaveen@gmail.com>
2025-09-25 12:21:24 +0530

    Create Structures.sv

---
+++ Practies codes/Structures.sv
@@ -0,0 +1,20 @@
+module structure;
+  typedef struct {
+    string name;
+    bit[31:0] wages;
+    integer proof;
+  } worker
+  
+  initial begin
+    worker m1,m2;
+    m1.name = "bolt";
+    m1.wages = 'h20000;
+    m1.proof = 'B11;
+    $display("worker m1 : %p",m1);
+    
+    m2.name = "ben";
+    m2.wages = 'h15000;
+    m2.proof = 'B12;
+    $display("worker m2 : %p", m2);
+  end
+endmodule
