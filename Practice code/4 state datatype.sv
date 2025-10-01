e41eff6e48810c8ee3c376c0decd5375921ab1ce
Naveengit12 <jikkilinaveen@gmail.com>
2025-10-01 12:22:31 +0530

    Create 4 state datatype.sv

---
+++ Practice code/4 state datatype.sv
@@ -0,0 +1,12 @@
+module four_state_datatype;
+  wire a;
+  reg b;
+  logic c;
+  time d;
+  integer e;
+  real f;
+  
+  initial begin
+    $display("a=%d, b=%d, c=%d, d=%0d, e=%d, f=%d", a, b, c, d, e, f);
+  end
+endmodule
