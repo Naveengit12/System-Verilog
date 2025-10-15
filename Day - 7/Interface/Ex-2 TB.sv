1bed3f7e3a5af0f2f743dd4df498e838eb9958ee
Naveengit12 <jikkilinaveen@gmail.com>
2025-10-15 17:56:38 +0530

    Update Ex-2 TB.sv

--- Day - 7/Interface/Ex-2 TB.sv
+++ Day - 7/Interface/Ex-2 TB.sv
@@ -24,4 +24,13 @@ module andgate_tb;
     $display("Ag.a=%b,Ag.b=%b,Ag.y=%0b", Ag.a,Ag.b,Ag.y);
   end
 endmodule
-  
+
+
+
+// output
+// # run -all
+// # Ag.a=0,Ag.b=0,Ag.y=0
+// # Ag.a=0,Ag.b=1,Ag.y=0
+// # Ag.a=1,Ag.b=0,Ag.y=0
+// # Ag.a=1,Ag.b=1,Ag.y=1
+// # exit
