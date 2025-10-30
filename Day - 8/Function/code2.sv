8b0afecc911f64c82a15cd37d1f0d04e878b997f
Naveen J <jikkilinaveen@gmail.com>
2025-10-30 17:00:11 +0530

    Create code2.sv

---
+++ Day - 8/Function/code2.sv
@@ -0,0 +1,16 @@
+// =======function arguments in declarations and mentioning directions========
+module sv_function;
+  int a;
+  
+  // function to add a 2 int values 
+  function int sum;
+    input int x,y;
+    sum = x + y;
+    //     $display("x=%0d,x=%0d,sum=%0d",x,y,sum);
+  endfunction
+  
+  initial begin
+    a = sum(19,16);
+    $display("a=%0d",a);
+  end
+endmodule
