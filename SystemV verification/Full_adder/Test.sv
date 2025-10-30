3d5f8d485fd470eae9e9644841f34bc18de10c45
Naveen J <jikkilinaveen@gmail.com>
2025-10-30 09:54:18 +0530

    Delete SystemV verification/Full_adder directory

--- SystemV verification/Full_adder/Test.sv
+++
@@ -1,9 +0,0 @@
-`include "environment.sv"
-
-program test(intf vif);
-  environment env;
-  initial begin
-    env = new(vif);
-    env.test_run();
-  end
-endprogram
--- SystemV verification/Full_adder/interface.sv
+++
@@ -1,8 +0,0 @@
-// Interface
-interface intf();
-  logic a;
-  logic b;
-  logic c_in;
-  logic sum;
-  logic c_out;
-  endinterface
