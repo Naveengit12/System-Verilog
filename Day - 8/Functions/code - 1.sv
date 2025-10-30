7ae457cc7fcf9eb388d58c26dc0690ba1693d741
Naveen J <jikkilinaveen@gmail.com>
2025-10-30 16:34:39 +0530

    Update code - 1.sv

--- Day - 8/Functions/code - 1.sv
+++ Day - 8/Functions/code - 1.sv
@@ -1,1 +1,15 @@
-
+// function arguments in parentheses
+module sv_function;
+  int a;
+  
+  // function to add a 2 int values 
+  function int sum(input int x,y);
+    sum = x + y;
+    //     $display("x=%0d,x=%0d,sum=%0d",x,y,sum);
+  endfunction
+  
+  initial begin
+    a = sum(4,5);
+    $display("a=%0d",a);
+  end
+endmodule
