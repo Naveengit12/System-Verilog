2eb0e5d9111f9824bed1ca227e80d6e4f3860c32
Naveen J <jikkilinaveen@gmail.com>
2025-11-07 18:01:33 +0530

    Create example-1.sv

---
+++ STATIC CLASS/Static Method/example-1.sv
@@ -0,0 +1,26 @@
+class packet;
+  byte packet_id;
+  static byte no_of_pkts_created;
+  
+  //constructor
+  function new();
+    no_of_pkts_created++;
+    packet_id = no_of_pkts_created;
+  endfunction
+  
+  //method to display class prperties
+  function void display(); 
+    $display("packet_id  = %0d",packet_id);
+  endfunction 
+endclass
+
+module static_properties;
+  packet pkt[5];
+
+  initial begin
+    foreach(pkt[i]) begin
+      pkt[i] = new();
+      pkt[i].display();
+    end
+  end  
+endmodule
