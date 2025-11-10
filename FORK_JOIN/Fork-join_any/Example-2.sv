2e40f483727f9316d5ff4bb07d8e4eb28d15d0dd
Naveen J <jikkilinaveen@gmail.com>
2025-11-10 16:16:48 +0530

    Create Example-2.sv

---
+++ FORK_JOIN/Fork-join_any/Example-2.sv
@@ -0,0 +1,24 @@
+
+module fork_join_any;
+
+  initial begin
+    $display("Starting fork-join_any");
+    
+    fork
+      begin
+        #100;
+        $display("Process 1 completed");
+      end
+      begin
+        #50;
+        $display("Process 2 completed");
+      end
+      begin
+        #80;
+        $display("Process 3 completed");
+      end
+    join_any
+
+    $display("One process completed. Continuing execution...");
+  end
+endmodule
