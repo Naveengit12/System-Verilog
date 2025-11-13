7327c4918523f5f4ac80b4723e8f23b1c85ac28a
Naveen J <jikkilinaveen@gmail.com>
2025-11-13 09:48:43 +0530

    Create 1.sv

---
+++ CLASSES/This-keyword/1.sv
@@ -0,0 +1,20 @@
+class cricket;
+  logic[10:0] teams;
+  int players,kit;
+  longint viewers;
+  
+  function new(logic[10:0]teams, int players, int kit, longint viewers);
+    this.teams = teams;
+    this.players = players;
+    this.kit = kit;
+    this.viewers = viewers;
+  endfunction
+endclass
+
+module class_cricket;
+  initial begin
+  cricket ckt;
+    ckt = new(5, 55, 5,101012301);
+    $display("teams=%0h,players=%0h,kit=%0h,viewers=%0h",ckt.teams,ckt.players,ckt.kit,ckt.viewers);
+  end
+endmodule
