10627b7729563d0c799c0e4ab29134cf221871c4
Naveen J <jikkilinaveen@gmail.com>
2025-11-29 15:11:11 +0530

    Update 1.sv

--- RANDOMIZATION/Soft_constraint/1.sv
+++ RANDOMIZATION/Soft_constraint/1.sv
@@ -1,1 +1,18 @@
+class parent;
+  rand bit [4:0] child;
+  constraint addr_data{soft child > 15; } 
+endclass
+
+module class_parent;
+  parent p;
+  initial begin
+   // packet pkt;
+    p = new();
+
+    repeat(5) begin
+      p.randomize() with{child < 25;};
+      $display("child = %0d",p.child);
+    end
+  end
+endmodule
 
