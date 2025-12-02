898da191b777824f10f29024f1cffdac92725b3d
Naveen J <jikkilinaveen@gmail.com>
2025-12-02 12:37:30 +0530

    Update 1.sv

--- RANDOMIZATION/Randcase/1.sv
+++ RANDOMIZATION/Randcase/1.sv
@@ -46,5 +46,37 @@ module class_marvel;
   end
 endmodule
   
-               
+
+
+// output
+// # run -all
+// # selected 10
+// # ** Warning: (vsim-3829) Non-existent associative array entry. Returning default value.
+// #    Time: 0 ns  Iteration: 0  Process: /class_marvel/#INITIAL#1089 File: design.sv Line: 1062
+// # selected 5
+// # ** Warning: (vsim-3829) Non-existent associative array entry. Returning default value.
+// #    Time: 0 ns  Iteration: 0  Process: /class_marvel/#INITIAL#1089 File: design.sv Line: 1057
+// # selected 25
+// # ** Warning: (vsim-3829) Non-existent associative array entry. Returning default value.
+// #    Time: 0 ns  Iteration: 0  Process: /class_marvel/#INITIAL#1089 File: design.sv Line: 1067
+// # selected 10
+// # selected 10
+// # selected 10
+// # selected 5
+// # selected 25
+// # selected 25
+// # selected 25
+// # probability for 5 = 0.125000 in 1 iteration
+// # god_thunder[5] = 2
+// # probability for god_thunder[5] = 0.200000
+// # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// # probability for 10 = 0.250000 in 1 iteration
+// # god_thunder[10] = 4
+// # probability for god_thunder[10] = 0.400000
+// # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// # probability for 25 = 0.625000 in 1 iteration
+// # god_thunder[25] = 4
+// # probability for god_thunder[25] = 0.400000
+// # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// # exit
     
